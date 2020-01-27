586b05e9b9a2c159c4d76bd78b8748827a5f32a0
Damien Grisonnet <damien.grisonnet@epita.fr>
2020-01-27 22:38:54 +0100

    instr: add instruction handlers

--- src/instruction.adb
+++ src/instruction.adb
@@ -11,9 +11,91 @@ package body instruction is
       return Op;
    end Fetch;
    
-   procedure Execute (Op : Opcode) is
+   procedure Execute (Cpu : Chip8; Op : Opcode) is
+      I : Integer;
+   begin
+      I := Integer(Shift_Right(Op, 12));
+      Instr_Handlers(I)(Cpu, Op);
+   end Execute;
+   
+   procedure Handler_0 (Cpu : Chip8; Op : Opcode) is
    begin
       null;
-   end Execute;
+   end Handler_0;
+   
+   procedure Handler_1 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_1;
+   
+   procedure Handler_2 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_2;
 
+   procedure Handler_3 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_3;
+   
+   procedure Handler_4 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_4;
+   
+   procedure Handler_5 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_5;
+   
+   procedure Handler_6 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_6;
+   
+   procedure Handler_7 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_7;
+   
+   procedure Handler_8 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_8;
+   
+   procedure Handler_9 (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_9;
+   
+   procedure Handler_A (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_A;
+   
+   procedure Handler_B (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_B;
+   
+   procedure Handler_C (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_C;
+   
+   procedure Handler_D (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_D;
+   
+   procedure Handler_E (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_E;
+   
+   procedure Handler_F (Cpu : Chip8; Op : Opcode) is
+   begin
+      null;
+   end Handler_F;
+   
 end instruction;
--- src/instruction.ads
+++ src/instruction.ads
@@ -1,10 +1,48 @@
 with Types; use Types;
+with Cpu; use Cpu;
 
 package instruction is
    
    subtype Opcode is Word;
    
    function Fetch (Mem : Memory; Addr : Address) return Opcode;
-   procedure Execute (Op : Opcode);
+   procedure Execute (Cpu : Chip8; Op : Opcode);
+   
+   procedure Handler_0 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_1 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_2 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_3 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_4 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_5 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_6 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_7 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_8 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_9 (Cpu : Chip8; Op : Opcode);
+   procedure Handler_A (Cpu : Chip8; Op : Opcode);
+   procedure Handler_B (Cpu : Chip8; Op : Opcode);
+   procedure Handler_C (Cpu : Chip8; Op : Opcode);
+   procedure Handler_D (Cpu : Chip8; Op : Opcode);
+   procedure Handler_E (Cpu : Chip8; Op : Opcode);
+   procedure Handler_F (Cpu : Chip8; Op : Opcode);
+   
+   type Instr_Handler is access procedure (Cpu : Chip8; Op : Opcode);
+   type Instr_Handler_Array is array (0 .. 15) of Instr_Handler;
+   
+   Instr_Handlers : constant Instr_Handler_Array := (Handler_0'Access,
+                                                     Handler_1'Access,
+                                                     Handler_2'Access,
+                                                     Handler_3'Access,
+                                                     Handler_4'Access,
+                                                     Handler_5'Access,
+                                                     Handler_6'Access,
+                                                     Handler_7'Access,
+                                                     Handler_8'Access,
+                                                     Handler_9'Access,
+                                                     Handler_A'Access,
+                                                     Handler_B'Access,
+                                                     Handler_C'Access,
+                                                     Handler_D'Access,
+                                                     Handler_E'Access,
+                                                     Handler_F'Access);
    
 end instruction;
