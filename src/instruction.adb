6e06b0b4241eada64f06ded98400dceb96df17d1
Damien Grisonnet <damien.grisonnet@epita.fr>
2020-01-27 23:30:50 +0100

    instr: add opcode variables

--- src/instruction.adb
+++ src/instruction.adb
@@ -5,9 +5,9 @@ package body instruction is
    function Fetch (Mem : Memory; Addr : Address) return Opcode is
       Op : Opcode;
    begin
-      Op := Unsigned_16(Mem(Addr));
+      Op := Word(Mem(Addr));
       Op := Shift_Left(Op, 8);
-      Op := Op + Unsigned_16(Mem(Addr + 1));
+      Op := Op + Word(Mem(Addr + 1));
       return Op;
    end Fetch;
    
@@ -19,6 +19,7 @@ package body instruction is
    end Execute;
    
    procedure Handler_0 (Cpu : Chip8; Op : Opcode) is
+      N : Word;
    begin
       case Op is
          when 16#00E0# =>
@@ -26,48 +27,69 @@ package body instruction is
          when 16#00EE# =>
             null;
          when others =>
-            null;
+            N := Op mod 16#1000#;
       end case;
    end Handler_0;
    
    procedure Handler_1 (Cpu : Chip8; Op : Opcode) is
+      N : Word;
    begin
-      null;
+      N := Op mod 16#1000#;
    end Handler_1;
    
    procedure Handler_2 (Cpu : Chip8; Op : Opcode) is
+      N : Word;
    begin
-      null;
+      N := Op mod 16#1000#;
    end Handler_2;
 
    procedure Handler_3 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      N : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      N := Op mod 16#100#;
    end Handler_3;
    
    procedure Handler_4 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      N : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      N := Op mod 16#100#;
    end Handler_4;
    
    procedure Handler_5 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      Y : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      Y := Op and 16#F0#;
    end Handler_5;
    
    procedure Handler_6 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      N : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      N := Op mod 16#100#;
    end Handler_6;
    
    procedure Handler_7 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      N : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      N := Op mod 16#100#;
    end Handler_7;
    
    procedure Handler_8 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      Y : Word;
    begin
-      case Op mod 16 is
+      X := Op and 16#F00#;
+      Y := Op and 16#F0#;
+      case Op mod 16#10# is
          when 0 =>
             null;
          when 1 =>
@@ -93,33 +115,46 @@ package body instruction is
    end Handler_8;
    
    procedure Handler_9 (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      Y : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      Y := Op and 16#F0#;
    end Handler_9;
    
    procedure Handler_A (Cpu : Chip8; Op : Opcode) is
+      N : Word;
    begin
-      null;
+      N := Op mod 16#1000#;
    end Handler_A;
    
    procedure Handler_B (Cpu : Chip8; Op : Opcode) is
+      N : Word;
    begin
-      null;
+      N := Op mod 16#1000#;
    end Handler_B;
    
    procedure Handler_C (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      N : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      N := Op mod 16#100#;
    end Handler_C;
    
    procedure Handler_D (Cpu : Chip8; Op : Opcode) is
+      X : Word;
+      Y : Word;
    begin
-      null;
+      X := Op and 16#F00#;
+      Y := Op and 16#F0#;
    end Handler_D;
    
    procedure Handler_E (Cpu : Chip8; Op : Opcode) is
+      X : Word;
    begin
-      case Op mod 256 is
+      X := Op and 16#F00#;
+      case Op mod 16#100# is
          when 16#9E# =>
             null;
          when 16#A1# =>
@@ -131,8 +166,10 @@ package body instruction is
    end Handler_E;
    
    procedure Handler_F (Cpu : Chip8; Op : Opcode) is
+      X : Word;
    begin
-      case Op mod 256 is
+      X := Op and 16#F00#;
+      case Op mod 16#100# is
          when 16#07# =>
             null;
          when 16#0A# =>
