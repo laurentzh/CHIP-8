53fc8a78fc67409df460d993aa57f3c65994b0ab
Damien Grisonnet <damien.grisonnet@epita.fr>
2020-01-27 21:30:40 +0100

    types: use interfaces

--- src/types.ads
+++ src/types.ads
@@ -1,10 +1,11 @@
+with Interfaces; use Interfaces;
+
 package types is
 
-type Nibble is mod 16 with Size => 4;
-type Byte is mod 256 with Size => 8;
-type Word is mod 65536 with Size => 16;
+   subtype Byte is Unsigned_8;
+   subtype Word is Unsigned_16;
 
-subtype Address is Integer range 0 .. 4095;
-type Memory is array (Address) of Byte;
+   subtype Address is Integer range 0 .. 4095;
+   type Memory is array (Address) of Byte;
 
 end types;
