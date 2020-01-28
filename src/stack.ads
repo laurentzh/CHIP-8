c3a5e54069b0da890eff1b75794721fabfa34d10
Laurent Zhu <laurent.zhu@epita.fr>
2020-01-28 17:22:16 +0100

    stack: add pre and post conditions

--- src/stack.ads
+++ src/stack.ads
@@ -1,18 +1,29 @@
+with Interfaces; use Interfaces;
 with Types; use Types;
 
 package Stack is
+   
    Stack_Capacity : constant Natural := 16;   
-   type Stack_Range is range 0 .. Stack_Capacity - 1;
+   subtype Stack_Range is Natural range 0 .. Stack_Capacity - 1;
    type Stack_Elements is array (Stack_Range) of Address;
 
    type Stack_Record is record
       Arr : Stack_Elements;
-      Size : Stack_Range;
+      Size : Natural;
    end record;
    
-   procedure Push_Stack(Stack : in out Stack_Record; Element : Address);
+   procedure Push_Stack(Stack : in out Stack_Record; Element : Address)
+     with Pre => Stack.Size < Stack_Capacity,
+     Post => Stack.Size <= Stack_Capacity
+     and then Stack.Size = Stack.Size'Old + 1;
    
-   function Pop_Stack(Stack : in out Stack_Record) return Address;
+   function Pop_Stack(Stack : in out Stack_Record) return Address
+     with Pre => Stack.Size > 0,
+     Post => Stack.Size = Stack.Size'Old - 1
+     and then Pop_Stack'Result = Stack.Arr(Stack.Size);
    
-   function Peek_Stack(Stack : Stack_Record) return Address;
+   function Peek_Stack(Stack : Stack_Record) return Address
+     with Pre => Stack.Size > 0,
+     Post => Peek_Stack'Result = Stack.Arr(Stack.Size - 1);
+   
 end Stack;
