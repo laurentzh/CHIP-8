7491fc1a0e3d7330f44dc077eb1a9e700999f705
Laurent Zhu <laurent.zhu@epita.fr>
2020-01-29 02:01:36 +0100

    main: add load_rom

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,18 @@
 with Cpu; use Cpu;
 with Instruction; use Instruction;
 with Stack; use Stack;
+with Roms; use Roms;
+with Types; use Types;
 
 procedure Main is
+
+   procedure Load_Rom(Cpu : in out Chip8; R : Rom) is
+   begin
+      for I in R'Range loop
+         Cpu.Mem(I) := R(I);
+      end loop;
+   end Load_Rom;
+
    Cpu : Chip8 := (Mem => (16#F0#, 16#90#, 16#90#, 16#90#, 16#F0#,
                            16#20#, 16#60#, 16#20#, 16#20#, 16#70#,
                            16#F0#, 16#10#, 16#F0#, 16#80#, 16#F0#,
@@ -30,6 +40,7 @@ procedure Main is
                    Keys => (others => False));
    Op : Opcode;
 begin
+   Load_Rom(Cpu, MAZE);
    loop
       Op := Fetch(Cpu);
       Execute(Cpu, Op);
